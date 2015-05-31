7f7346fed0f8c52cc159d792d6b4c4a1bb173f47
muaad <muadh24@gmail.com>
2015-05-31 19:28:24 +0300

    Addded read to some Message

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531153353) do
+ActiveRecord::Schema.define(version: 20150531162433) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -58,9 +58,10 @@ ActiveRecord::Schema.define(version: 20150531153353) do
     t.integer  "contact_id"
     t.boolean  "incoming",   default: true
     t.integer  "user_id"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.integer  "account_id"
+    t.boolean  "read",       default: false
   end
 
   add_index "messages", ["account_id"], name: "index_messages_on_account_id"
