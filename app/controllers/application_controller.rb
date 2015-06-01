f7350895b3cf9c0646ed5392bfc62aeb038687f2
muaad <muadh24@gmail.com>
2015-06-01 21:29:32 +0300

    Added method to format time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :time_formatter
+
+  def time_formatter time
+  	"#{time.strftime("%d/%m/%Y")} #{time.strftime("%I:%M%p")}"  	
+  end
 end
