bc41e8327e047cf517616388e8f11ef244600c95
muaad <muadh24@gmail.com>
2015-06-01 21:17:00 +0300

    Angular JS file

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,11 @@
 (function() {
 	var app = angular.module('sms', []);
+
+	app.controller('ContactsController', ['http', function($http) {
+		var x = this;
+		x.contacts = [];
+		$http.get('/contacts.json').succes(function(data) {
+			x.contacts = data;
+		})
+	}]);
 })();
