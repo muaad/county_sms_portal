881954051295c58eb6c6fb3ab26f83b211531231
muaad <muadh24@gmail.com>
2015-06-02 00:52:20 +0300

    Added method to fetch conversation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,11 @@
 class Message < ActiveRecord::Base
 	scope :incoming, -> { where(incoming: true) }
 	scope :outgoing, -> { where(incoming: false) }
-	
+
   belongs_to :contact
   belongs_to :user
+
+  def self.conversations
+  	Contact.all.collect{|c| c.messages.incoming.last if !c.messages.empty?}
+  end
 end
