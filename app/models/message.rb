19f84dfed81052c9832eebb068972c491d888660
muaad <muadh24@gmail.com>
2015-06-02 00:21:18 +0300

    Added incoming and outgoing scopes to Message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ActiveRecord::Base
+	scope :incoming, -> { where(incoming: true) }
+	scope :outgoing, -> { where(incoming: false) }
+	
   belongs_to :contact
   belongs_to :user
 end
