bcd6fb6734b89a6daa967c86e77b81e3ba537c00
muaad <muadh24@gmail.com>
2015-06-02 00:53:09 +0300

    Load inbox from messages in the DB

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,8 @@ class MessagesController < ApplicationController
   # GET /messages
   # GET /messages.json
   def index
-    @messages = Message.all
+    @messages = Message.all#incoming.order(created_at: :desc)
+    @conversations = Message.conversations
   end
 
   # GET /messages/1
