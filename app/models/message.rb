ff38b1df959764bcde34c710eccad531d7619033
muaad <muadh24@gmail.com>
2015-06-04 21:34:06 +0300

    added method to get sender of message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,21 @@
 class Message < ActiveRecord::Base
 	scope :incoming, -> { where(incoming: true) }
 	scope :outgoing, -> { where(incoming: false) }
+	scope :unread, -> { where(read: false) }
 
   belongs_to :contact
   belongs_to :user
 
   def self.conversations
-  	Contact.all.collect{|c| c.messages.incoming.last if !c.messages.empty?}
+  	Contact.all.collect{|c| c.messages.incoming.last if !c.messages.empty?}.compact
+  end
+
+  def sender
+  	from = nil
+  	if incoming
+  		from = contact
+  	else
+  		from = user
+  	end
   end
 end
