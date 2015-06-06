06ab0cfa37c690d20a015da00c7ddc64f7bf533c
muaad <muadh24@gmail.com>
2015-06-06 23:54:10 +0300

    Route for conversation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :groups
 
 	get "/contacts/upload" => "contacts#upload_page", as: "contacts_upload_page"
-	get "/conversation/:contact_id/" => "messages#conversation", as: "conversation"
+	get "/conversations/:contact_id/" => "messages#conversation", as: "conversation"
 	post "/contacts/delete_multiple" => "contacts#delete_multiple", as: "delete_multiple_contacts"
 	post "/upload_contacts" => "contacts#bulk_upload", :as => "upload_contacts", via: [:post]
 	post "/add_to_class" => "contacts#add_to_group", :as => "add_to_group", via: [:post]
