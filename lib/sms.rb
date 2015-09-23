0c1a9c94a49074c927346274e102a3e70ab843b2
muaad <muadh24@gmail.com>
2015-09-23 16:38:25 +0300

    Split long messages before sending them

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,4 +1,4 @@
-require 'AfricasTalkingGateway'
+# require 'AfricasTalkingGateway'
 class SMS
 	def initialize
     @user = ENV['SMS_GATEWAY_USER']
@@ -9,30 +9,21 @@ class SMS
     @channel = ENV['SMS_CHANNEL']
   end
 
+  def split_message message
+  	message.chars.each_slice(160).map(&:join)
+  end
+
   def send to, message
   	`curl -H "Content-Type: application/x-www-form-urlencoded; charset=utf-8" -d "username=#{@user}&password=#{@password}&MSISDN=#{to}&content=#{message}&channel=#{@channel}&shortcode=#{@shortcode}&campaignid=#{@campaign_id}&premium=1&multitarget=1" #{@url}`
-    # if !Rails.env.production?
-    #   `curl -H "Content-Type: application/x-www-form-urlencoded; charset=utf-8" -d "username=#{@user}&password=#{@password}&MSISDN=#{to}&content=#{message}&channel=#{@channel}&shortcode=#{@shortcode}&campaignid=#{@campaign_id}&premium=1&multitarget=1" #{@url}`
-    # else
-    #   puts "<>>>>>> TARGET: INFO\nMSISDN: #{to}\nTEXT: #{message}"
-    # end
   end
-	# def initialize
-	# 	@gateway = AfricasTalkingGateway.new('muaad', ENV['SMS_API_KEY'])
-	# end
+	
 	def send_message text, phone_number
-		send phone_number, text
+		segments = [message]
+		segments = split_message(message)
+
+		segments.each do |text|
+			send phone_number, text
+		end
 		Message.create! contact: Contact.find_by(phone_number: phone_number), text: text, incoming: false
-	# 	begin
-	# 	  reports = @gateway.sendMessage(phone_number, text)
-		  
-	# 	  reports.each {|x|
-	# 	    # status is either "Success" or "error message"
-	# 	    puts 'number=' + x.number + ';status=' + x.status + ';messageId=' + x.messageId + ';cost=' + x.cost
-	# 	  }
-	# 	  Message.create! contact: Contact.find_by(phone_number: phone_number), text: text, incoming: false
-	# 	rescue AfricasTalkingGatewayException => ex
-	# 	  puts 'Encountered an error: ' + ex.message
-	# 	end
 	end
 end
